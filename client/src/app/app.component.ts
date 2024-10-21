5728dede7b6a36575f0a4d3eb6a86dd3bf258a30
tudor <niculaetudor94@gmail.com>
2024-10-21 17:17:12 +0300

    Angular Added

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = '.Net Angular Demo';
+  users: any;
+  error: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => this.error = error.message,
+      complete: () => console.log('Request Completed!')
+    })
+  }
+}
