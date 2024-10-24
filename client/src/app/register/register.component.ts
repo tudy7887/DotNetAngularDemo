cb7a8fe58e3df84b9aa6d819dee360bf4b13547b
tudor <niculaetudor94@gmail.com>
2024-10-24 18:17:43 +0300

    routing + theme added

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, input, output, } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,17 +14,16 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
+  private router = inject(Router);
   //userFromHomeComponent = input.required<any>();
   cancelRegister = output<boolean>();
   model: any = {};
 
   register() {
     this.accountService.register(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: output => this.toastr.error(output.error)
     });
   }
 
